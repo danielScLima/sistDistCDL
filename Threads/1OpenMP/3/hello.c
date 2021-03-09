b82e4ade2962bfe67081e915758617cb6d038406
Daniel Lima <daniel.lima@larces.uece.br>
2021-03-09 20:57:58 -0300

    Exemplo 3 com openmp

---
+++ Threads/1OpenMP/3/hello.c
@@ -0,0 +1,50 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NROWS 5
+#define NCOLS 3
+
+int soma(int mat[][NCOLS], int id)
+{
+	int i;
+	int s = 0;
+	for(i=0;i<NCOLS;++i)
+	{
+		s += mat[id][i];
+	}
+	return s;
+}
+
+int main (int argc, char *argv[]) 
+{
+	int nthreads, tid;
+
+	int mat[NROWS][NCOLS] = {{1,2,3},{4,5,6},{7,8,9},{10,11,12},{13,14,15}};
+
+	omp_set_num_threads(NROWS+1);
+	//se não usar, fica 1 thread por cada cpu
+
+	// Fork a team of threads giving them their own copies of variables
+	
+	#pragma omp parallel private(nthreads, tid)
+	{
+	  	tid = omp_get_thread_num();              	// Obtain thread number
+  		printf("Hello World from thread = %d\n", tid);
+
+	 	if (tid == 0) // Only master thread does this
+	 	{
+			nthreads = omp_get_num_threads();
+			printf("Number of threads = %d\n", nthreads);
+	  	}
+	  	else
+	  	{
+	  		int ret = soma(mat, tid-1);
+	  		printf("Soma na thread %d: %d\n", tid, ret);
+	  	}
+	  	
+	} // All threads join master thread and disband
+	
+	return(0);
+}
+
