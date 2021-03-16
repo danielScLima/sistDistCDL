b0c4a5203ea0c7d6f5701ab2b520eb1d8bb29fc8
Daniel Lima <daniel.lima@larces.uece.br>
2021-03-16 19:07:50 -0300

    uso do system

---
+++ Processes/3System/1/code.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#include <iostream>
+
+int main()
+{
+      system("ls");
+      
+      //A linha abaixo é executada
+      printf ("Eu ainda nao estou morto\n");
+      
+      return 0;
+}
